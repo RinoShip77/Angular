fd042aa0da322d960237c2d037d20119d13543dd
Olivier Bourgault <77986350+Olivier1875009@users.noreply.github.com>
2023-09-11 23:49:32 -0400

    update (again of profile)
    New:
    Modal for updating the password

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { User } from '../modele/User';
 import { ElectrolibService } from '../electrolib.service';
-import { formatDate } from '@angular/common';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-profile',
@@ -10,15 +10,20 @@ import { formatDate } from '@angular/common';
 })
 export class ProfileComponent {
   visible = false;
-  fileName = '';
+  profilePicture = '';
   user: User = new User();
+  password = {
+    oldPassword: '',
+    newPassword: '',
+    confirmationNewPassword: ''
+  }
 
   @Output() disconnected = new EventEmitter<User>();
 
   //-------------------------------------------------------
   // Function to display every book in the database
   //-------------------------------------------------------
-  constructor(private electrolib: ElectrolibService) { }
+  constructor(private electrolib: ElectrolibService, private modalService: NgbModal) { }
 
   //-------------------------------------------------------
   // Function to display every book in the database
@@ -31,34 +36,26 @@ export class ProfileComponent {
   }
 
   //-------------------------------------------------------
-  // Function to disconnect a user
-  //-------------------------------------------------------
-  disconnect() {
-    this.visible = false;
-    this.disconnected.emit(this.user);
-  }
-
-  //-------------------------------------------------------
   // Function to upload a new profile picture
   //-------------------------------------------------------
   uploadImage() {
     console.log('new profile picture');
   }
-  
+
   //-------------------------------------------------------
   // Open the inventory of this user
   //-------------------------------------------------------
   openInventory() {
     console.log('open inventory');
   }
-  
+
   //-------------------------------------------------------
   // Open the borrow(s) mades by the user
   //-------------------------------------------------------
   openBorrows() {
     console.log('open borrows');
   }
-  
+
   //-------------------------------------------------------
   // Open the favorites of the user
   //-------------------------------------------------------
@@ -67,10 +64,65 @@ export class ProfileComponent {
   }
 
   //-------------------------------------------------------
+  // Open the modal to update the user password
+  //-------------------------------------------------------
+  open(content: any) {
+    this.modalService.open(content, {
+      animation: true,
+      centered: true,
+      keyboard: true,
+      size: 'lg'
+    });
+  }
+
+  //-------------------------------------------------------
+  // Function to upload a new profile picture
+  //-------------------------------------------------------
+  updatePassword() {
+    console.log('update password ...\n');
+    console.log(this.password);
+  }
+
+  //-------------------------------------------------------
+  // Function to upload a new profile picture
+  //-------------------------------------------------------
+  formatPostalCode() {
+    console.log('change postal code');
+
+    if(this.user.postalCode.length >= 3) {
+      this.user.postalCode = this.user.postalCode.slice(0, 3) + ' ' + this.user.postalCode.slice(3);
+    }
+  }
+  
+  //-------------------------------------------------------
+  // Function to upload a new profile picture
+  //-------------------------------------------------------
+  formatPhoneNumber() {
+    console.log('change phone number');
+    
+    // if(this.user.phoneNumber.length == 3) {
+    //   this.user.phoneNumber = this.user.phoneNumber.slice(0, 3) + '-' + this.user.phoneNumber.slice(3);
+    // }
+    
+    // if(this.user.phoneNumber.length == 7) {
+    //   this.user.phoneNumber = this.user.phoneNumber.slice(3, 7) + '-' + this.user.phoneNumber.slice(7);
+    // }
+  }
+  
+  //-------------------------------------------------------
   // Function to upload a new profile picture
   //-------------------------------------------------------
   updateProfile() {
     console.log('update profile ...\n');
     console.log(this.user);
+    console.log(this.profilePicture);
+  }
+
+  //-------------------------------------------------------
+  // Function to disconnect a user
+  //-------------------------------------------------------
+  disconnect() {
+    this.visible = false;
+    this.disconnected.emit(this.user);
   }
 }
