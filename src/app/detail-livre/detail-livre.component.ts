cb4433e40c3e44131db4d628939be6a4f1d110f3
Vincent Lavoie Whitlock <1966857@cstj.qc.ca>
2023-09-23 20:03:06 -0400

    update de détailsLivre

--- src/app/detail-livre/detail-livre.component.ts
+++ src/app/detail-livre/detail-livre.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ElectrolibService } from '../electrolib.service';
 import { ActivatedRoute } from '@angular/router';
+import { Book } from '../modele/Book';
 
 @Component({
   selector: 'app-detail-livre',
@@ -11,12 +12,16 @@ export class DetailLivreComponent {
   constructor(private electrolibSrv: ElectrolibService,private route: ActivatedRoute){
 
   }
+  book: Book = new Book();
 
   //au lancement de la page on vachercher les parametres (ici id), dans la lamda qui contient les params on lance la recherche dans la bd avec le service
   ngOnInit() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     if(id){
-      this.electrolibSrv.getBook(id);
+     this.electrolibSrv.getBook(id).subscribe(receivedBook =>{
+      this.book=receivedBook;
+      console.log(this.book);
+     });
     }
   }
 
