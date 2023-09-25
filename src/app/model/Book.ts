1826e59e5f3e72a5917ede08e5f59acd3dfaf373
Olivier Bourgault <77986350+Olivier1875009@users.noreply.github.com>
2023-09-25 16:18:07 -0400

    update

--- src/app/model/Book.ts
+++ src/app/model/Book.ts
@@ -3,7 +3,9 @@ import { Author } from "./Author";
 export class Book {
   idBook: number = 0;
   idGenre: number = 0;
+  genre: Genre = new Genre();
   idAuthor: number = 0;
+  author: Author = new Author();
   title: string = '';
   description: string = '';
   isbn: string = '';
@@ -14,6 +16,7 @@ export class Book {
   publishedDate: Date = new Date();
   originalLanguage: string = '';
  // evaluations: Evaluation[] = new Array();
+ isFavorite: boolean = false;
  // favorites: Favorite[] = new Array();
   //reservations: Reservation[] = new Array();
 }
