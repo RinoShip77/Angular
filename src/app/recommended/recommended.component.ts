802dc2dd7842410ded2831973c39a671e0316630
VincentLavoieWhitlock <89476781+VincentLavoieWhitlock@users.noreply.github.com>
2023-09-24 22:40:29 -0400

    ajout du carrousel dans recommended

--- src/app/recommended/recommended.component.ts
+++ src/app/recommended/recommended.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./recommended.component.css']
 })
 export class RecommendedComponent {
+  
+  currentSlide:number=0;
+
+  public slides = [
+    {src: "https//image1"},
+    {src: "https//image2"},
+    {src: "https//image3"},
+    {src: "https//image4"},
+  ];
+
+  onPreviousClick() {
+    const previous = this.currentSlide - 1;
+    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
+    console.log("previous clicked, new current slide is: ", this.currentSlide);
+  }
+
+  onNextClick() {
+    const next = this.currentSlide + 1;
+    this.currentSlide = next === this.slides.length ? 0 : next;
+    console.log("next clicked, new current slide is: ", this.currentSlide);
+  }
 
 }
