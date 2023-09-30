927111771b9b2f58e68a2b47baeafffa365b8122
Nicola Labelle <labellenicola@gmail.com>
2023-09-30 16:35:32 -0400

    Correction boucle infinie
    Retiré l'instanciation d'un objet 'borrow' d'un 'book' parce que ça causait une boucle infinie.

--- src/app/model/Book.ts
+++ src/app/model/Book.ts
@@ -14,13 +14,13 @@ export class Book {
   title: string = '';
   description: string = '';
   isbn: string = '';
- borrow: Borrow = new Borrow();
+  //borrow: Borrow = new Borrow();
   isBorrowed: boolean = false;
   cover: string = "/";
   publishedDate: string = '';
   originalLanguage: string = '';
- evaluations: Evaluation[] = new Array();
- isFavorite: boolean = false;
- favorites: Favorite[] = new Array();
-  reservations: Reservation[] = new Array();
+  //evaluations: Evaluation[] = new Array();
+  isFavorite: boolean = false;
+  //favorites: Favorite[] = new Array();
+  //reservations: Reservation[] = new Array();
 }
